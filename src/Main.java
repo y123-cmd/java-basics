68df0a2571fb991da4c164543136643ed0a66ea5
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-11-27 13:18:25 +0300

    modified the main file by adding method to main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+public  class Main {
+    static void myMethod() {
         int sum = 0;
-        for (int i = 0; i <= 100; i = i + 2) {
-          sum = sum + i;
-
-            System.out.println("final sum :"+ sum);
-            System.out.println(i);
+        for (int i = 0; i <= 100; ++i) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
 
         }
+        System.out.println(sum);
+        }
+    public static void main(String[]args){
+        myMethod();
     }
 }
