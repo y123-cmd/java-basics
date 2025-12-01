8d1ce9de3b039c03a1828cd95fb813ed744b3fb2
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-12-01 15:22:22 +0300

    added datatypes

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ public  class Main {
         for (int i = 0; i <= 100; ++i) {
             if (i % 2 == 0) {
                 sum = sum + i;
-            }
 
+            }
         }
         System.out.println(sum);
         }
     public static void main(String[]args){
         myMethod();
     }
+
 }
+
+
+
+
